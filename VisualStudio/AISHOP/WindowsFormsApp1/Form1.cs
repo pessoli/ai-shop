91d0d83c653d3bb41202486f102702464e16825c
pessoli <pessolilucas@gmail.com>
2024-11-18 12:20:48 -0300

    connecting and inserting values into database

--- VisualStudio/AISHOP/WindowsFormsApp1/Form1.cs
+++ VisualStudio/AISHOP/WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Npgsql;
 
 namespace WindowsFormsApp1
 {
@@ -16,7 +17,7 @@ namespace WindowsFormsApp1
 
         string originalFilePath = @"C:\Users\luk\Desktop\ai-pattern\config.json";
         string pythonPath = @"C:\Users\luk\Desktop\ai-pattern\python/main.py";
-        string connectionString = "Host=localhost;Username=postgres;Password=admin;Database=shop";
+        string connectionString = "Host=localhost;Username=postgres;Password=admin;Database=postgres";
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -45,6 +46,17 @@ namespace WindowsFormsApp1
                                 if (process != null)
                                 {
                                     process.WaitForExit();
+
+                                    content = File.ReadAllText(originalFilePath);
+
+                                    jsonObject = JObject.Parse(content);
+
+                                    string video_path = (string)jsonObject["video_path"];
+                                    int personFound = (int)jsonObject["dudes_visible"];
+                                    Console.WriteLine(personFound);
+                                    DateTimeOffset time = DateTimeOffset.Now;
+                                    DateTime timestamp= time.UtcDateTime;  //converts to UTC timestamp, commonly used in brazil
+                                    connectAndInsert(video_path, personFound, timestamp);
                                     Console.WriteLine("O script Python terminou de ser executado.");
                                 }
                             }
@@ -77,5 +89,35 @@ namespace WindowsFormsApp1
                 }
             }
         }
+
+        public void connectAndInsert(String fileName, int personFound, DateTime timestamp)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão com o banco de dados estabelecida!");
+
+                    string sql = "INSERT INTO video (video_path, max_people, recording_time) VALUES (@valor1, @valor2, @valor3)";
+
+                    using (var cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        // Adicionar parâmetros para evitar SQL Injection
+                        cmd.Parameters.AddWithValue("valor1", fileName);
+                        cmd.Parameters.AddWithValue("valor2", personFound);
+                        cmd.Parameters.AddWithValue("valor3", timestamp);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
+            }
+        }
     }
 }
+
+
